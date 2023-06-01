641ee1c8cd4fb47a0fee58f65dde2116d31ccd21
Vaclav Hajsman (a.k.a. COOKIE) <93037688+COOKIE-1816@users.noreply.github.com>
2023-06-01 16:20:23 +0200

    Add files via upload

---
+++ javascript/cs-03_material-11_priklad-1.js
@@ -0,0 +1,7 @@
+let x;
+
+x = 3 + 2;      console.log("3 + 2 je",                 x);
+x = 42 - 1.50;  console.log("42 - 1,5 je",              x);
+x = 3 * 11;     console.log("3 krát 11 je",             x);
+x = 12 / 3;     console.log("12 děleno 3 je",           x);
+x = 42 % 9;     console.log("Zbytek po dělení 42 9 je", x);
