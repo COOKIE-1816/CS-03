c92eaba766e45e1ba0886c4c60fc23c8c1c2a9b0
Vaclav Hajsman (a.k.a. COOKIE) <93037688+COOKIE-1816@users.noreply.github.com>
2023-06-07 19:43:30 +0200

    Add files via upload

---
+++ javascript/cs-03_material-14_priklad-1.js
@@ -0,0 +1,11 @@
+var a = 2;
+var b = 21;
+
+switch(a * b) {
+    case 42:
+        console.log("Čtyřicet dva");
+    case 22:
+        console.log("Dvacet dva");
+    default:
+        console.log("Neznámý výsledek");
+}
---
+++ javascript/cs-03_material-14_priklad-2.js
@@ -0,0 +1,14 @@
+var a = 2;
+var b = 21;
+
+switch(a * b) {
+    case 42:
+        console.log("Čtyřicet dva");
+        break;
+    case 22:
+        console.log("Dvacet dva");
+        break;
+    default:
+        console.log("Neznámý výsledek");
+        break;
+}
